0ffb35cded61eb2bc284cbaaf8489018cc7fe2d0
LEAS <aluno@unipampa.edu.br>
2026-06-16 11:20:23 -0300

    Prompt 4

--- docker/gateway/entrypoint.sh
+++ docker/gateway/entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 echo "==> Configurando interface LAN (eth1)"
 ip link set eth1 up
 
-# Atribui IP estático se ainda não configurado
 if ! ip addr show eth1 | grep -q "${GATEWAY_LAN_IP:-192.168.100.1}"; then
     ip addr add ${GATEWAY_LAN_IP:-192.168.100.1}/24 dev eth1
     echo "IP ${GATEWAY_LAN_IP:-192.168.100.1}/24 adicionado à eth1"
@@ -12,12 +11,18 @@ else
     echo "IP já configurado em eth1"
 fi
 
+echo "==> Habilitando IP forwarding"
+sysctl -w net.ipv4.ip_forward=1
+
+echo "==> Aplicando regras nftables (NAT)"
+nft -f /etc/nftables/ruleset.nft
+
 echo "==> Gerando configuração do Kea a partir do template"
 envsubst < /kea-dhcp4.conf.template > /etc/kea/kea-dhcp4.conf
 
 echo "==> Iniciando Kea DHCP Server em background"
 kea-dhcp4 -c /etc/kea/kea-dhcp4.conf &
 
-echo "==> Gateway pronto. DHCP ativo na LAN."
+echo "==> Gateway pronto. NAT ativo e DHCP rodando."
 # Manter container vivo
 tail -f /dev/null
